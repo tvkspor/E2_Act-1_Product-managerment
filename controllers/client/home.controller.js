cda2e9766431d56875556f1b2477378c3d2844e4
tvkspor <tmtuan120704@gmail.com>
2024-08-12 11:35:43 +0700

    Fix error

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,4 +1,20 @@
 // [GET] /client/home
-module.exports.index = (req, res) => {
-  res.render("client/pages/home/index");
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+  }).sort({ position: "desc" });
+
+  const newProducts = products.map((item) => {
+    item.priceNew = parseFloat(
+      ((item.price * (100 - item.discountPercentage)) / 100).toFixed(0)
+    );
+    return item;
+  });
+
+  // [GET] /client/products
+  res.render("client/pages/products/index", {
+    pageTitle: "Danh sach san pham",
+    products: newProducts,
+  });
 };
